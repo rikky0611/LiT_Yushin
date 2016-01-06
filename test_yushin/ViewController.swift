8012d8b9697f9c75bed0e8f81c0bc8fe86a93410
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-01-06 22:31:27 +0900

    Initial Commit

---
+++ test_yushin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_yushin
+//
+//  Created by 荒川陸 on 2016/01/06.
+//  Copyright © 2016年 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
